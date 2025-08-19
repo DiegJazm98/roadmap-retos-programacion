c7c4c0e659cbd31195c171c6942230004e82df4f
DiegJazm98 <69829009+DiegJazm98@users.noreply.github.com>
2025-08-19 16:43:27 -0500

    Create DiegJazm98.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/DiegJazm98.py
@@ -0,0 +1,19 @@
+#https://www.python.org
+#comentario en una linea
+""" esto también es un comentario en varias lineas"""
+'''
+Esto tambien es un comentario
+en varias lineas 
+'''
+my_variable = "mi variable"
+my_variable = "nuevo valor de mi variable"
+MY_CONSTANT = "mi constante"
+
+my_int= 1
+my_float= 1.5
+my_bool = True
+my_bool = False
+my_string = "mi cadena de texto"
+my_other_string = "Mi otra cadena de texto"
+
+print("Hola, Python")
